47208a2e6f392977e32cd7ee016e8879dbd2c6e6
Gimenez Federico <fede_888_55@hotmail.com>
2024-11-26 23:44:35 -0300

    base de la tienda

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 
-
-    let producto1 = "computadora"
-    let producto2 = "placa de video"
-    let producto3 = "cables sata"
-  
 let userNameValidation = false
 do{
     let nombreCliente = prompt ("Por favor, ingrese su nombre")
@@ -14,5 +9,48 @@ do{
         userNameValidation = false
         alert("No ingreso un nombre")
     }
-}while (userNameValidation == false );
+}while (userNameValidation == false )
+
+function tienda (){
+    alert("Bienvenido a la tienda")
+    let check;
+    let producto;
+    do{
+        let opciones = prompt ("Para comprar una computadora ingrese 1\nPara comprar un celular ingrese 2\nPara comprar un monitor ingrese 3")
+        opciones = parseInt(opciones, 10)
+        switch(opciones){
+            case 1:
+                check = confirm("¿Quiere comprar una computadora?");
+                break;
+        }
+    }while (check == false)
+};
+
+function servicios (){
+    alert("servicios")
+
+};
+
+let control;
+do{
+    if (userNameValidation == true){
+        let opciones = prompt ("Elija 1 para proceder a la tienda o 2 para ver los servicios")
+        opciones = parseInt(opciones, 10)
+        switch(opciones){
+            case 1:
+                control = true
+                tienda()
+                break;
+            case 2:
+                control = true
+                servicios()
+                break;
+            default:
+                alert("Opción no valida")
+                control = false
+                break;
+            }
+    }
+
+}while(control == false);
 
